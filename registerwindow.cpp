0a11bb2814d562ff2e9db0a0e0743dacf1098846
Marwan <marwan_i@aucegypt.edu>
2024-03-02 23:27:25 +0200

    change3h

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -19,3 +19,33 @@ RegisterWindow::~RegisterWindow()
 {
     delete ui;
 }
+
+void RegisterWindow::on_register_2_clicked()
+{
+    QString username = ui->username1->text() ;
+    QString password = ui->password1->text() ;
+    QString repassword = ui->repassword->text() ;
+    QString day1 =ui->day->text() ;
+    QString year1 = ui->year->text() ;
+    QString  month =ui-> monthcombo ->currentText() ;
+
+    Qstring gender ;
+    if(ui->male->isChecked())
+    {
+        gender = "Male" ;
+    } else if(ui->female->isChecked())
+    {
+        gender="Female" ;
+    }
+
+    QString account ;
+    if(ui->user->isChecked())
+    {
+        account = "User" ;
+    }
+    else if(ui->admin->isChecked())
+    {
+        account = "Admin" ;
+    }
+}
+
